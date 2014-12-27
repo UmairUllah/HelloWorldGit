894dcc15adb9c69adc54e5accc7373170726c8f5
UmairUllah <umairullah11@yahoo.com>
2014-12-27 14:20:36 +0500

    First Implementation of Hello World Program

---
+++ HelloWorldGit/src/HelloWorld.java
@@ -0,0 +1,15 @@
+
+public class HelloWorld {
+	
+	/*
+	 * A simple Hello World Java Program
+	 * 
+	 * @author Umair Ullah
+	 */
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
